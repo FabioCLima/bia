8f215ef854779127842b3f73b807e5563c16ea2d
Fabio Carvalho Lima <lima.fisico@gmail.com>
2026-08-31 17:29:05 -0300

    test: quebra proposital para validar branch protection (task 003, sera revertido)

--- tests/unit/controllers/versao.test.js
+++ tests/unit/controllers/versao.test.js
@@ -17,7 +17,8 @@ describe('Versao Controller', () => {
     // Chama o método get do objeto controller
     get(req, res);
 
-    expect(res.send).toHaveBeenCalledWith('Bia 4.2.0');
+    // teste quebrado propositalmente (task 003 - validação de branch protection, será revertido)
+    expect(res.send).toHaveBeenCalledWith('valor-propositalmente-errado');
   });
 
   test('get deve retornar a string de resposta correta quando VERSAO_API não está definido', () => {
